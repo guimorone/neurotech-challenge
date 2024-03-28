56ea70b23840e1a6df1f79016b7a377b25e1764a
Guilherme <guimorone@gmail.com>
2024-03-27 21:09:53 -0300

    Criando endpoint para pegar cotação atual

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,8 +2,14 @@ from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from forex_python.converter import CurrencyRates
+
+c = CurrencyRates()
 
 
-class API(APIView):
-    def get(self, request: Request) -> Response:
-        return Response("Hello, world!", status=status.HTTP_200_OK)
+class CurrencyView(APIView):
+    def get(self, _request: Request) -> Response:
+        return Response(
+            {"USD": [("BRL", c.get_rate("USD", "BRL"))], "BRL": [("USD", c.get_rate("BRL", "USD"))]},
+            status=status.HTTP_200_OK,
+        )
